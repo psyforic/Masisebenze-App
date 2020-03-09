fbc24bbd49168090298fdaa95b439c4a79cc7e57
Lundi Mapundu <lundi@developmenthub.co.za>
2020-03-09 17:03:17 +0200

    latest

--- src/app/admin/lawfirms/clients/work-information/work-information.component.ts
+++ src/app/admin/lawfirms/clients/work-information/work-information.component.ts
@@ -1,7 +1,13 @@
-import { finalize } from 'rxjs/operators';
+import { element } from 'protractor';
+import { MatOptionSelectionChange } from '@angular/material';
+
+import { finalize, startWith, map } from 'rxjs/operators';
 import {
   WorkAssessmentServiceProxy, WorkContextDto, ClientDetailOutput,
-  ClientServiceProxy
+  ClientServiceProxy,
+  WorkContextSummaryDto,
+  SummaryReponseDto,
+  OccupationDto
 } from './../../../../../shared/service-proxies/service-proxies';
 import { Component, OnInit, Injector, ElementRef } from '@angular/core';
 import { AppComponentBase } from '@shared/app-component-base';
@@ -9,20 +15,34 @@ import { ActivatedRoute } from '@angular/router';
 import { TagModel } from 'ngx-chips/core/accessor';
 import { Observable, of } from 'rxjs';
 import { FormControl } from '@angular/forms';
+export class MaxDataValue {
+  elementId: string;
+  elementName: string;
+  title: string;
+  dataValue: number;
+  category: number;
+}
 @Component({
   selector: 'app-work-information',
   templateUrl: './work-information.component.html',
   styleUrls: ['./work-information.component.scss'],
   providers: [WorkAssessmentServiceProxy]
 })
+
 export class WorkInformationComponent extends AppComponentBase implements OnInit {
   client: ClientDetailOutput = new ClientDetailOutput();
   clientId = '';
   defaultAge = [];
   ageList: string[] = [];
   workContextList: WorkContextDto[] = [];
+  workContextSummaryList: WorkContextSummaryDto[] = [];
+  responses: SummaryReponseDto[] = [];
+  occupations: OccupationDto[] = [];
+  maxDataValues: MaxDataValue[] = [];
   jobTitle: FormControl = new FormControl();
-  company: FormControl = new FormControl();
+  occupation: FormControl = new FormControl();
+  myControl = new FormControl();
+  filteredOptions: Observable<OccupationDto[]>;
   isLoading = false;
   workConextDto: WorkContextDto = new WorkContextDto();
   constructor(injector: Injector,
@@ -37,6 +57,11 @@ export class WorkInformationComponent extends AppComponentBase implements OnInit
 
   ngOnInit() {
     this.getClient();
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
   }
   getClient() {
     this.isLoading = true;
@@ -48,6 +73,23 @@ export class WorkInformationComponent extends AppComponentBase implements OnInit
         this.client = result;
       });
   }
+  getOccupations(keyword, start: number, end: number) {
+    this.occupations = [];
+    this.isLoading = true;
+    this._workAssessmentService.getOccpations(keyword, start, end)
+      .pipe(finalize(() => {
+        this.isLoading = false;
+      }))
+      .subscribe(result => {
+        this.occupations = result;
+        // console.log(this.occupations);
+      });
+  }
+  getWorkContext(event: MatOptionSelectionChange, onetSocCode) {
+    if (event.source.value != null && onetSocCode != null) {
+      this.getElementNames(event.source.value);
+    }
+  }
   getElementNames(keyword) {
     this.ageList = [];
     this.isLoading = true;
@@ -57,27 +99,65 @@ export class WorkInformationComponent extends AppComponentBase implements OnInit
       }))
       .subscribe(result => {
         if (result != null && result.length > 0) {
+          this.getWorkContextSummary(result[0].onetSocCode);
           this.workContextList = result;
-          result.forEach((workContext) => {
-            if (this.ageList.indexOf(workContext.elementName) === -1) {
-              this.ageList.push(workContext.elementName);
-            }
-          });
+          result.filter(x => x.elementID === '4.C.2.d.1.a' || x.elementID === '4.C.2.d.1.b' ||
+            x.elementID === '4.C.2.d.1.c' || x.elementID === '4.C.2.d.1.d' || x.elementID === '4.C.2.d.1.e'
+            || x.elementID === '4.C.2.d.1.f' || x.elementID === '4.C.2.d.1.g' || x.elementID === '4.C.2.d.1.h' ||
+            x.elementID === '4.C.2.d.1.i')
+            .forEach((workContext) => {
+              const dataValues: number[] = [];
+              if (this.ageList.indexOf(workContext.elementName) === -1) {
+                result.filter(x => x.elementID === workContext.elementID)
+                  .forEach((i) => {
+                    dataValues.push(i.dataValue);
+                  });
+                if (workContext.dataValue === Math.max.apply(null, dataValues)) {
+                  const maxDataValue = new MaxDataValue();
+                  maxDataValue.dataValue = Math.max.apply(null, dataValues);
+                  maxDataValue.title = workContext.title;
+                  maxDataValue.elementId = workContext.elementID;
+                  maxDataValue.elementName = workContext.elementName;
+                  maxDataValue.category = workContext.category;
+                  // console.log(dataValues);
+                  this.maxDataValues.push(maxDataValue);
+                  this.ageList.push(workContext.elementName);
+                }
+              }
+            });
+
         }
       });
   }
   search() {
-    this.getElementNames(this.jobTitle.value);
+    this.getOccupations(this.jobTitle.value, 1, 1000);
+    // this.getElementNames(this.jobTitle.value);
   }
-  getCategories(elementName){
+  getWorkContextSummary(onetSocCode) {
+    this.isLoading = true;
+    this._workAssessmentService.getWorkContextSummary(onetSocCode)
+      .pipe(finalize(() => {
+        this.isLoading = false;
+      })).subscribe(result => {
+        this.workContextSummaryList = result;
+        // console.log(result);
+      });
+  }
+  getCategories(elementName) {
     return this.workContextList.filter(w => w.elementName === elementName);
   }
   onAgesAdd(tag: any): Observable<TagModel> {
     this.defaultAge.push(tag.value);
+    this.workContextList.filter(x => x.elementName === tag.value);
     return of(tag);
   }
   onAgesRemove(tag: any): Observable<TagModel> {
     this.defaultAge.splice(this.defaultAge.indexOf(tag.value));
     return of(tag);
+  }
+  private _filter(value: string): OccupationDto[] {
+    const filterValue = value.toLowerCase();
+
+    return this.occupations.filter(option => option.title.toLowerCase().includes(filterValue));
   }
 }
